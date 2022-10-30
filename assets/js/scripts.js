6e33af4ed247b1e7321713e2991e729744f3822e
smldgweb <sales.ldgwebdesign.com>
2022-10-30 10:25:03 -0400

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+const modalBg = document.getElementById('modal-bkg');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileIcon = document.getElementById('mobile-icon');
+
+function openMenu(){
+    mobileIcon.classList.toggle('close');
+    modalBg.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+}
+
+mobileIcon.addEventListener('click', openMenu);
+
